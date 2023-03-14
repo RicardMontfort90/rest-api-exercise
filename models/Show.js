ab8c6fbf2e9dff7246c3ce0226456a988bade16a
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-14 20:39:09 +0100

    work in progress

--- models/Show.js
+++ models/Show.js
@@ -1,19 +1,8 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
 
-const showSchema = new Schema(
-
-    {
-        title: "The Simpsons",
-        creator: "Matt Groening",
-        launched: 1989,
-        genre: "comedy",
-        image: "https://ychef.files.bbci.co.uk/976x549/p02fc1jw.jpg",
-        description: "The series is a satirical depiction of American life, epitomized by the Simpson family, which consists of Homer, Marge, Bart, Lisa, and Maggie. The show is set in the fictional town of Springfield and parodies American culture and society, television, and the human condition."
-    },
-    {
-        timestamps: true
-    }
-)
-
-const Show = model("Show", showShcema);
+const showSchema = new Schema ({
+    
+})
+const Show = mongoose.model("Show", showShcema);
 module.export = Show;
