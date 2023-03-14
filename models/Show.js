07147728c182f6f55a037e575ae3fcc1633a3d5d
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-14 20:40:13 +0100

    work in progress

--- models/Show.js
+++ models/Show.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const showSchema = new Schema ({
-    
-})
+    title: {
+        type: String,
+        required: [true, 'Show needs to have a title']
+    },
+    creator: {
+        type: String,
+    },
+    launched: {
+        type: Number
+    },
+    genre: {
+        type: String
+    },
+    image: {
+        type: String,
+    },
+    description: {
+        type: String,
+        default: 'No description provided'
+    },
+}, 
+{
+    timestamps: true
+});
+
 const Show = mongoose.model("Show", showShcema);
 module.export = Show;
