0f566fde3569885bd7ac2be66a170e2b179d46bb
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-14 20:53:59 +0100

    work in progress

--- db/seed.js
+++ db/seed.js
@@ -4,7 +4,16 @@ mongoose.set('strictQuery', true);
 const Show = require('../models/Show');
 
 
-// Import data to seed
+data = [
+  {
+    title: "The Simpsons",
+    creator: "Matt Groening",
+    launched: 1989,
+    genre: "comedy",
+    image: "https://ychef.files.bbci.co.uk/976x549/p02fc1jw.jpg",
+    description: "The series is a satirical depiction of American life, epitomized by the Simpson family, which consists of Homer, Marge, Bart, Lisa, and Maggie. The show is set in the fictional town of Springfield and parodies American culture and society, television, and the human condition."
+  }
+]
 
 mongoose
   .connect(process.env.MONGO_URL)
